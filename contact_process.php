03532e1d3a7131ee5591153fa84b5d8b1b04c8cb
Vihanga Gaganatharu <vihangagaganatharu1@gmail.com>
2024-06-26 17:31:24 +0530

    contact section & all are completed

--- contact_process.php
+++ contact_process.php
@@ -1,6 +1,6 @@
 <?php
 
-    $to = "rockybd1995@gmail.com";
+    $to = "vihangagaganatharu1@gmail.com";
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
     $subject = $_REQUEST['subject'];
